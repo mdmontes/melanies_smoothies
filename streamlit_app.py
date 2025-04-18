0a25a6c0f045d24dd3c04f5db29515f0a5bfd9a4
Manny Montes <37914939+mdmontes@users.noreply.github.com>
2025-04-18 16:25:59 -0400

    Update streamlit_app.py with smoothiefroot
    Updating script to include smoothiefroot api.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Own Smoothie! :cup_with_straw:")
@@ -45,4 +46,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success(f'Your Smoothie is ordered {name_on_order}', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
