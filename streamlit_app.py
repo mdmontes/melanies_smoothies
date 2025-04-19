2ba2358e4710c03ae020d66e758f0ee9d386b0bb
Manny Montes <37914939+mdmontes@users.noreply.github.com>
2025-04-18 20:24:53 -0400

    Updating to review data type error again

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         ingredients_string+=fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         search_on_type = type(search_on)
-        # st.write(f'data type for search on is {search_on_type}')
+        st.write(f'data type for search on is {search_on_type}')
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         if type(search_on_type) != type('string'):
           search_on = 'fruit not found'
