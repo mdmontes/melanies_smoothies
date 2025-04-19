f2d86d807e43bd5c510568ada54722da51627b9a
Manny Montes <37914939+mdmontes@users.noreply.github.com>
2025-04-18 20:18:47 -0400

    Adding f string to search_on_type

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         ingredients_string+=fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         search_on_type = type(search_on)
-        st.write('data type for search on is' + search_on_type)
+        st.write(f'data type for search on is {search_on_type}')
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
