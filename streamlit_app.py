bd441dd0c3a514e3b730b213f5925e8855e57bb2
Manny Montes <37914939+mdmontes@users.noreply.github.com>
2025-04-20 18:46:57 -0400

    updating search to is None
      updating search to is None

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,9 @@ if ingredients_list:
         search_on_type = type(search_on)
         st.write(f'data type for search on is {search_on_type}')
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        if f'{search_on_type}' == f"<class 'NoneType'>":
-          st.writen(f'error triggered, if statement triggered')
-          search_on = fruit_chosen
+      if search_on is None:
+        st.write('error triggered, if statement triggered')
+        search_on = fruit_chosen
       
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
